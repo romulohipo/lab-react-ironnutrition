a087762608df9adff246c3ddd28e367ab31f3f35
Romulo Hipolito <hipolito.romulo12@gmail.com>
2021-02-06 14:03:58 -0300

    to-be-continued

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import foods from './foods.json';
 import FoodBox from './Components/FoodBox/FoobBox';
+import NewFood from './Components/NewFood/NewFood';
 
 function App() {
   return (
-    <div className ='App'>
-      {foods.map(food => 
-      <FoodBox 
-      name={food.name} 
-      image={food.image} 
-      calories={food.calories} 
-      quantity={food.quantity} />
-      )}      
+    <div className='App'>
+
+      <button>Add Food</button>
+      <NewFood />
+      <div className='column is-half'>
+        {foods.map(food =>
+          <FoodBox
+            name={food.name}
+            image={food.image}
+            calories={food.calories}
+            quantity={food.quantity} />
+        )}
+        <NewFood></NewFood>
+        </div>
     </div>
-  );
+  )
+
 }
+
 
 export default App;
---
+++ src/Components/NewFood/NewFood.js
@@ -0,0 +1,30 @@
+import React, { Components } from 'react'
+
+class NewFood extends Component() {
+    // super(props)
+    // foodName = { value: '' };
+    // foodImage = { value: '' };
+    // foodCalories = { value: '' };
+    // quantity = 0;
+
+    render() {
+        return (
+            'Hello World'
+            // <form onSubmit={this.handleSubmit}>
+            //     <label>Food Name:</label>
+            //     <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+            //     <label>Food Image:</label>
+            //     <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+            //     <label>Food Calories:</label>
+            //     <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+            //     <input type="submit" value="Submit" />
+            // </form>
+        );
+    }
+
+}
+
+export default NewFood
